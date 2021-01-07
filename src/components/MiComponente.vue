f1abb30369af7fbf82ef50ce1bacd7a43ad4c940
Matias Rojas <maigrojas.dev@gmail.com>
2021-01-07 12:03:09 -0300

    Agregando un componente nuevo llamado Peliculas.vue, actualizando el Header.vue

--- src/components/MiComponente.vue
+++ src/components/MiComponente.vue
@@ -8,6 +8,23 @@
       <h2>Reactividad</h2>
       <input type="text" v-model="nombre" />
       <p>Mi nombre es: <strong>{{nombre}}</strong></p>
+      <hr />
+
+      <h2>Direcctivas condicionales</h2>
+      <input type="number" v-model="edad" />
+      <p>Mi edad es: <strong>{{edad}}</strong></p>
+
+      <h2>Condicionales</h2>
+      <strong v-if="edad < 0" class="mensajeDanger" >Edad no puede ser menor a 0</strong>
+      <strong v-else-if="edad >= 0 && edad < 18" class="mensajeDanger">Eres menor de edad</strong>
+      <strong v-else class="mensajeDanger">Eres mayor de edad</strong>
+
+      <h2>Directiva iterativa: for</h2>
+      <ol>
+        <li v-for="pelicula in peliculas" :key="pelicula" >
+          {{pelicula}}
+        </li>
+      </ol>
     </div>
   </section>
 </template>
@@ -20,7 +37,14 @@ export default {
     return {
       titulo: "Mi primer componente",
       subtitulo: "Soy el subtítulo de Mi Componente",
-      nombre: ""
+      nombre: "",
+      edad: 18,
+      peliculas: [
+        'Batman',
+        'Hobbit',
+        'Spiderman',
+        'Trolls'
+      ]
     };
   },
 };
